aa73572908741dd650d8a5d5d73e3534fa30460a
Houtan <62800782+mast9rmind@users.noreply.github.com>
2023-11-28 16:50:03 +0330

    Update script.js
    [Feat] make script being usable inside tampermonkey

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-document.querySelector("#chat_expanded_inner").childNodes.forEach((node) => {
-  node.childNodes.forEach((childNode) => validate(node, childNode));
-});
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Try to get rid of currupt staff
+// @author       Focused Something
+// @match        https://atwar-game.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=atwar-game.com
+// @grant        none
+// ==/UserScript==
 
-const observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    mutation.addedNodes.forEach(function (node) {
-      node.childNodes.forEach((child) => validate(node, child));
+
+
+
+function banUserList() {
+    'use strict';
+    // exact name of the players you wish to ban
+    const NAME_LIST = ['Ghostface', 'Conscious Huarck']
+
+
+
+    document.querySelector("#chat_expanded_inner").childNodes.forEach((node) => {
+        node.childNodes.forEach((childNode) => validate(node, childNode));
     });
-  });
-});
 
-function validate(parent, child) {
-  if (
-    child.nodeName === "A" &&
-    child.textContent &&
-    child.textContent.includes("Pavle")
-  ) {
-    parent.style.display = "none";
-  }
+    const observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            mutation.addedNodes.forEach(function (node) {
+                node.childNodes.forEach((child) => validate(node, child));
+            });
+        });
+    });
+
+    function validate(parent, child) {
+        if (
+            child.nodeName === "A" &&
+            child.textContent &&
+            NAME_LIST.some((n) => child.textContent.includes(n))
+        ) {
+            parent.style.display = "none";
+        }
+    }
+
+    observer.observe(document.querySelector("#chat_expanded_inner"), {
+        childList: true,
+    });
 }
 
-observer.observe(document.querySelector("#chat_expanded_inner"), {
-  childList: true,
-});
+window.addEventListener('load', banUserList, false);
